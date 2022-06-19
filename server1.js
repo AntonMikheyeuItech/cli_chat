3c4c016a1d3bba55a7128edf5b55789a371dd621
Anton Mikheyeu <anton.mikheyeu@itechart-group.com>
2022-06-19 12:22:58 +0300

    tmp

--- server1.js
+++ server1.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+const { Readable } = require('stream');
+const { consumer_ip } = require('./settings.json');
+const stdin = process.openStdin();
+
+const rs = new Readable();
+rs.setEncoding('utf8');
+rs._read = () => {};
 
 const app = express();
 
@@ -15,5 +23,13 @@ app.post('/stream', req => {
 });
 
 app.listen('5001', () => {
-  console.log('I am here!');
+  console.log(`Talk with ${consumer_ip}`);
+});
+
+axios.post(`http://${consumer_ip}:5002/stream`, rs)
+
+stdin.addListener("data", text => {
+  rs.resume();
+  rs.push(text);
+  rs.pause();
 });
--- server2.js
+++ server2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const { Readable } = require('stream');
+const { consumer_ip } = require('./settings.json');
 const stdin = process.openStdin();
 
 const rs = new Readable();
@@ -12,16 +13,24 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, _res, next) => {
+  req.setEncoding('utf8');
+  next();
+});
+
+app.post('/stream', req => {
+  req.pipe(process.stdout);
+});
+
 app.listen('5002', () => {
-  console.log('I am here!');
+  console.log(`Talk with ${consumer_ip}`);
 });
 
-axios.post('http://localhost:5001/stream', rs);
+// let sender;
 
-stdin.addListener("data", text => {
-  rs.resume();
-  rs.push(text);
-  rs.pause();
-});
-
-
+// stdin.addListener("data", text => {
+//   if (!sender) sender = axios.post(`http://${consumer_ip}:5001/stream`, rs);
+//   rs.resume();
+//   rs.push(text);
+//   rs.pause();
+// });
